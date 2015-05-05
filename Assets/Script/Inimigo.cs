80becdb72cc4e0b737e95b778db9ec2fe4532248
Gabriel <developer.gabriel@gmail.com>
2015-05-05 14:31:49 -0300

    First commit

---
+++ Assets/Script/Inimigo.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inimigo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		NavMeshAgent agente = GetComponent<NavMeshAgent> ();
+		GameObject fimDoCaminho = GameObject.Find ("FimDoCaminho");
+		Vector3 posicaoDoFimDoCaminho = fimDoCaminho.transform.position;
+		agente.SetDestination ( posicaoDoFimDoCaminho );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Script/Torre.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Torre : MonoBehaviour {
+
+	public GameObject projetilPrefab;
+	private float momentoDoUltimoDisparo;
+
+	[Range(0, 3)]
+	public float tempoDeRecarga = 1f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Atira ();
+	}
+
+	void Atira() 
+	{
+		float tempoAtual = Time.time;
+		if (tempoAtual > momentoDoUltimoDisparo + tempoDeRecarga) {
+			momentoDoUltimoDisparo = tempoAtual;
+			GameObject pontoDeDisparo = this.transform.Find ("CanhaoDaTorre/PontoDeDisparo").gameObject;
+			Vector3 posicaoDoPontoDeDisparo = pontoDeDisparo.transform.position;
+			Instantiate (projetilPrefab, posicaoDoPontoDeDisparo, Quaternion.identity);
+		}
+	}
+
+	void Update()
+	{
+		Atira ();
+	}
+}
